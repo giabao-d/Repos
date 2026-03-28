1fe883d6e1e0e8dc00175fff5a579f34092fa992
giabao-d <giabaodtu120@gmail.com>
2026-03-28 10:15:27 +0700

    Add files via upload

---
+++ Code Nhom.java
@@ -0,0 +1,52 @@
+package svngido;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SVNgido {
+    private static ArrayList<String> tasks = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int choice;
+        System.out.println("--- QUAN LY CONG VIEC NHOM ---");
+
+        do {
+            System.out.println("\n1. Them moi");
+            System.out.println("2. Xem danh sach");
+            System.out.println("3. Xoa du lieu");
+            System.out.println("4. Chinh sua du lieu");
+            System.out.println("5. Thoat");
+            System.out.print("Chon: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            if (choice == 1) {
+                addTask(); 
+            } else if (choice == 2) {
+                showTasks(); 
+            } else if (choice == 3) {
+                deleteTask(); 
+            } else if (choice == 4) {
+                editTask();
+            }
+        } while (choice != 5);
+    }
+
+
+    public static void addTask() {
+        
+    }
+
+    public static void showTasks() {
+        
+    }
+
+    public static void deleteTask(){
+        
+    }
+
+    public static void editTask() {
+        
+    }
+}
